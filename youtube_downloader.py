c26fccce48f29aa5b7c135bb93452be6a588e324
alham__aa <alhamazhar4@gmail.com>
2023-05-17 23:20:20 +0530

    script will add thumbnails to mp3 files
    Now the script has ability to insert thumbnails to mp3 files and also some minor bug fixes

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 
 '''This Python script allows you to download videos and audios from YouTube by simply entering the URL of the video.
@@ -7,16 +6,15 @@ The script has a colorful terminal output.
 *** curently this will only work on only linux as intended '''
 
 
-import subprocess
+from subprocess import run, check_output, DEVNULL, CalledProcessError, STDOUT
+from os import name, path, uname, chdir, rename
 import sys
 from pytube import YouTube
 import clipboard
-from os import getlogin as user_name
-import os
 
 
 def color(color):
-    '''ansi color codes to get colored output on termainl'''
+    '''ansi color codes to get colored output on terminal'''
 
     colors = {'reset': '\033[0m',
               'red': '\033[31m',
@@ -36,34 +34,75 @@ def color(color):
     return str(string)
 
 
-class ytube():
+class YTube():
     try:
         def __init__(self, link):
             self.link = link
             self.yt = YouTube(link)
 
         def title(self):
+            '''Fetching the Youtube video caption of given url'''
             yt = self.yt
             return f'{color(["bold","blue"])}Title: {color(["reset","blue","italic"])}{yt.title}{color(["reset"])}'
-    except:
-        print(f'{color(["red"])}an error occurred!{color(["reset"])}')
+    except Exception as e:
+        print(
+            f'{color(["red"])}An error occurred while fetching Title: as {e}{color(["reset"])}')
 
     def download_path(self):
-        '''determine the download path on each operating system to download and if couldn't download on current working direcotory'''
+        '''determine the path of default download directory path on each operating system to download and if couldn't download on current working directory'''
 
-        if os.name == 'nt':
-            download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+        if name == 'nt':
+            download_path = path.join(path.expanduser('~'), 'Downloads')
 
-        elif os.name == 'posix' and 'darwin' in os.uname().sysname.lower():
-            download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+        elif name == 'posix' and 'darwin' in uname().sysname.lower():
+            download_path = path.join(path.expanduser('~'), 'Downloads')
 
-        elif os.name == 'posix' and 'linux' in os.uname().sysname.lower():
-            download_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+        elif name == 'posix' and 'linux' in uname().sysname.lower():
+            download_path = path.join(path.expanduser('~'), 'Downloads')
 
         else:
             download_path = '.'
 
+        chdir(download_path)
         return download_path
+
+    def thumbnail(self, mp3_filename):
+        '''Fetches the thumbnail from the given URL and insert the thumbnail into mp3 file'''
+        yt = self.yt
+
+        def download_thumbnail(output_filename):
+            '''Downloads the thumbnail'''
+            try:
+                thumbnail_url = yt.thumbnail_url
+                run(['wget', thumbnail_url, '-O', output_filename],
+                    stdout=DEVNULL, stderr=DEVNULL)
+
+            except Exception as e:
+                print(
+                    f'An error occurred while downloading the thumbnail: {e}')
+
+        def insert_thumbnail(mp3_filename, thumbnail_file):
+            ''''add the thumbnail to the mp3 file'''
+            try:
+
+                # we can't add the thumbnail to existing file and save, so we have to create a temporary file
+                temp_file = 'temp.mp3'
+                run(['ffmpeg', '-i', mp3_filename, '-i', thumbnail_file, '-map', '0', '-map', '1', '-c',
+                               'copy', '-id3v2_version', '3', '-y', temp_file], stdout=DEVNULL, stderr=DEVNULL)
+
+                # after inserting the thumbnail moving image and original files into trash folder and renaming temporary file to original name
+                run(['gio', 'trash', thumbnail_file])
+                run(["gio", "trash", mp3_filename])
+                rename(temp_file, mp3_filename)
+
+            except FileNotFoundError as e:
+                print(f'File not found: {e}')
+            except Exception as e:
+                print(f'An error occurred while inserting thumbnail as: {e}')
+
+        # gets mp3 files name as parameter to insert the thumbnail
+        download_thumbnail(mp3_filename + '.jpg')
+        insert_thumbnail(mp3_filename, mp3_filename + '.jpg')
 
     def Video(self):
         '''if wanted to download the youtube video processes for that will happen here'''
@@ -89,11 +128,14 @@ class ytube():
         print()
 
         for stream in streams:
-            print(f'\t{color(["bold","red"])}{stream}. {color(["reset","green"])}{streams[stream]}')
+            print(
+                f'\t{color(["bold","red"])}{stream}. {color(["reset","green"])}{streams[stream]}')
 
-        video_format = input(f'choose a video stream to download.(press enter for 360p): ')
+        video_format = input(
+            f'choose a video stream to download.(press enter for 360p): ')
 
-        print(f'downloading{color(["white","bold"])}.{color(["blue"])}.{color(["green"])}.{color(["reset","green"])}')
+        print(
+            f'downloading{color(["white","bold"])}.{color(["blue"])}.{color(["green"])}.{color(["reset","green"])}')
 
         if video_format == '':
             video_format = '3'
@@ -102,53 +144,58 @@ class ytube():
 
         video.download(self.download_path())
 
-        print(f'downloaded on {color(["blue","italic"])}{self.download_path()}')
-
-        sys.exit()
-
-    def Audio(self):
-        '''if want to download only audio from youtube video processes for that will happen here'''
-        
-        yt = self.yt
-        print(f'downloading{color(["white","bold"])}.{color(["blue"])}.{color(["green"])}.{color(["reset","green"])}')
-
-        audio = yt.streams.get_audio_only()
-
-        file_location = str(audio.download(self.download_path())).replace(' ', '\x20')
-
-        mp3_file_locaton = file_location[:file_location.rfind('.')]+'.mp3'
-
-        try:
-
-            '''when we try to download audio version through pytube we can\'t download as mp3. so in here converting mp4 into mp3'''
-
-            ffmpeg_cmd = ["ffmpeg", "-i", file_location, "-vn", "-acodec","libmp3lame", "-b:a", "192k", "-loglevel", "error", mp3_file_locaton]
-            subprocess.check_output(ffmpeg_cmd, stderr=subprocess.STDOUT)
-            subprocess.run(["gio", "trash", file_location])
-
-            print('Successfully converted to mp3')
-
-        except subprocess.CalledProcessError as e:
-            print('Error during converting mp4 to mp3:', e.output.decode())
-
         print(
             f'downloaded on {color(["blue","italic"])}{self.download_path()}')
 
         sys.exit()
 
+    def Audio(self):
+        '''if want to download only audio from youtube video processes for that will happen here'''
+
+        yt = self.yt
+        print(
+            f'downloading{color(["white","bold"])}.{color(["blue"])}.{color(["green"])}.{color(["reset","green"])}')
+
+        audio = yt.streams.get_audio_only()
+
+        file_location = str(audio.download(
+            self.download_path())).replace(' ', '\x20')
+
+        mp3_file_location = f"{file_location[:file_location.rfind('.')]}.mp3"
+
+        try:
+            '''when we try to download audio version through pytube we can\'t download as mp3. so in here converting mp4 into mp3'''
+
+            ffmpeg_cmd = ["ffmpeg", "-i", file_location, "-vn", "-acodec",
+                          "libmp3lame", "-b:a", "192k", "-loglevel", "error", mp3_file_location]
+            check_output(ffmpeg_cmd, stderr=STDOUT)
+            run(["gio", "trash", file_location])
+
+            print('Successfully converted to mp3')
+
+            # inserting the thumbnail to the downloaded mp3 file
+            self.thumbnail(mp3_file_location)
+
+        except CalledProcessError as e:
+            print('Error during converting mp4 to mp3:', e.output.decode())
+
+        print(
+            f'downloaded on {color(["blue","italic"])}{self.download_path()}')
+
 
 def welcome():
     print(f'{color(["bold","green"])}Youtube {color(["underlined","italic"])}Video/Audio{color(["reset","bold","green"])} Downloader by {color(["white","italic"])}Alham_{color(["red"])}\U0000270C{color(["reset"])}')
 
-    link = str(input(f'enter url {color(["italic"])}(enter to copy from clipboard){color(["reset"])}: '))
-    
+    link = str(input(
+        f'enter url {color(["italic"])}(enter to copy from clipboard){color(["reset"])}: '))
+
     if link == '':
         link = clipboard.paste()
 
-    utube = ytube(link)
+    utube = YTube(link)
 
     print(utube.title())
-    
+
     def decision():
         decs = input(f'{color(["red","bold"])}\
     1. {color(["reset","green"])}Video{color(["reset"])}\n{color(["red","bold"])}\
@@ -156,10 +203,13 @@ def welcome():
         return decs
 
     decs = str(decision())
+
     if decs == '1':
         utube.Video()
     elif decs == '2' or decs == '':
         utube.Audio()
+        sys.exit()
+
     else:
         decs = decision()
         if decs == '1':
@@ -172,9 +222,9 @@ def welcome():
 
 def check_internet_connection():
     try:
-        output = subprocess.check_output(['ping', '-c', '1', 'google.com'])
+        output = check_output(['ping', '-c', '1', 'google.com'])
         return True
-    except subprocess.CalledProcessError:
+    except CalledProcessError:
         return False
 
 
@@ -189,9 +239,9 @@ if check_internet_connection():
         print('\naborting!')
         sys.exit()
     except KeyError:
-        print('invalied stream number')
+        print('invalid stream number')
     except Exception as e:
         print(f'{color(["red"])}an error occurred!{color(["reset"])}')
         print(e)
 else:
-    print('you don\'t hava an active internet connection')
+    print('you don\'t have an active internet connection')
